75a5bc2d09def4f3fd4f7c35967997ee88f20e80
Avani Patel <anpatel36@yahoo.in>
2021-04-08 21:27:19 +0530

    [Display the user input to the list]

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+
+  const[enterdGoal, setEnteredGoal] = useState('');
+  const[courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enterdText) => {
+    setEnteredGoal(enterdText);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [
+      ...courseGoals, 
+      {id: Math.random().toString(), value: enterdGoal}
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input} 
+          onChangeText={goalInputHandler}
+          value = {enterdGoal}
+          />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+
+      <FlatList 
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals} 
+        renderItem={ itemData=> (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+        />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: { 
+    padding: 50 
   },
+  inputContainer: { 
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center' 
+  },
+  input: { 
+    borderColor: 'black', 
+    width: '80%', 
+    borderWidth: 1, 
+    padding: 10 
+  },
+  listItem: { 
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
