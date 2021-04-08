74a22a1240c0379a55f22e03f88ba6f329321d48
Avani Patel <anpatel36@yahoo.in>
2021-04-08 23:14:25 +0530

    Passing Data between components

--- App.js
+++ App.js
@@ -1,58 +1,41 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from 'react-native';
+
 import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
 
-  const[enterdGoal, setEnteredGoal] = useState('');
-  const[courseGoals, setCourseGoals] = useState([]);
-
-  const goalInputHandler = (enterdText) => {
-    setEnteredGoal(enterdText);
-  }
-
-  const addGoalHandler = () => {
+  const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
-      ...courseGoals, 
-      {id: Math.random().toString(), value: enterdGoal}
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
     ]);
   };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input} 
-          onChangeText={goalInputHandler}
-          value = {enterdGoal}
-          />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
-
-      <FlatList 
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
         keyExtractor={(item, index) => item.id}
-        data={courseGoals} 
-        renderItem={ itemData=> <GoalItem title={itemData.item.value}/>}
-        />
-
+        data={courseGoals}
+        renderItem={itemData => <GoalItem title={itemData.item.value} />}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: { 
-    padding: 50 
+  screen: {
+    padding: 50
   },
-  inputContainer: { 
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    alignItems: 'center' 
-  },
-  input: { 
-    borderColor: 'black', 
-    width: '80%', 
-    borderWidth: 1, 
-    padding: 10 
-  }
 });
